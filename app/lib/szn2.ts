1cc263e6211b0d7e65f5d46709081d4091a5cbf4
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-04-03 14:03:04 +0100

    Update szn2.ts

--- app/lib/szn2.ts
+++ app/lib/szn2.ts
@@ -118,6 +118,15 @@ export const szn2 = [
   },
   {
     image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/59f3c39a9fd270e89fc2b4c3f9567feb9bb8d075fd161e54cf0844f06bc9e7ca.jpg",
+    name: "FreedOM Fight",
+    url: "https://app.manifold.xyz/c/MeansSzn2Card14",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/18/",
+    startDate: "2023-04-03",
+    endDate: "2023-07-03",
+  },
+  {
+    image:
       "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/a25dbcc594af8105e4ebc8d3180de02f61644f0a331807c8a3bcd4efc5f06405.jpg",
     name: "47 Means-in-1",
     url: "https://app.manifold.xyz/c/Means47N1Szn1",
