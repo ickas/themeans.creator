449972949d41d421423f3253d14af9d06342d6fb
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-04-07 00:31:50 +0100

    Update szn2.ts

--- app/lib/szn2.ts
+++ app/lib/szn2.ts
@@ -136,6 +136,15 @@ export const szn2 = [
   },
   {
     image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/c62ba66b80051ec4383f5e0b039b7ef72e878063d7201f1ef7fc385a5ee326e2.jpg",
+    name: "NFTland Parade",
+    url: "hhttps://app.manifold.xyz/c/MeansSzn2Card16",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/21/",
+    startDate: "2023-04-07",
+    endDate: "2023-07-07",
+  },
+  {
+    image:
       "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/a25dbcc594af8105e4ebc8d3180de02f61644f0a331807c8a3bcd4efc5f06405.jpg",
     name: "47 Means-in-1",
     url: "https://app.manifold.xyz/c/Means47N1Szn1",
