c61ddba935a5fddcbc846cb20ed68ff0a4e679a3
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-04-19 10:42:05 +0100

    Update szn2.ts

--- app/lib/szn2.ts
+++ app/lib/szn2.ts
@@ -181,6 +181,15 @@ export const szn2 = [
   },
   {
     image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/7a0965eae1bd8b6e05a22bb96772833eb3034e0348d3a138c0d86f44b5f62886.jpg",
+    name: "Survive Mode",
+    url: "https://app.manifold.xyz/c/MeansSzn2Card21",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/26/",
+    startDate: "2023-04-19",
+    endDate: "2023-07-19",
+  },
+  {
+    image:
       "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/a25dbcc594af8105e4ebc8d3180de02f61644f0a331807c8a3bcd4efc5f06405.jpg",
     name: "47 Means-in-1",
     url: "https://app.manifold.xyz/c/Means47N1Szn1",
