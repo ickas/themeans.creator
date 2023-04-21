c2620a0674bdcabaea5ce84cf1aaa4cf66229130
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-04-21 10:41:27 +0100

    Update szn2.ts

--- app/lib/szn2.ts
+++ app/lib/szn2.ts
@@ -190,6 +190,15 @@ export const szn2 = [
   },
   {
     image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/549ca110f040efec10d5809f8984ab6e0c9a9d95de15160bc9538ef61a313656.avif",
+    name: "Unchained to the System",
+    url: "https://app.manifold.xyz/c/MeansSzn2Card22",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/27/",
+    startDate: "2023-04-21",
+    endDate: "2023-07-21",
+  },
+  {
+    image:
       "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/a25dbcc594af8105e4ebc8d3180de02f61644f0a331807c8a3bcd4efc5f06405.jpg",
     name: "47 Means-in-1",
     url: "https://app.manifold.xyz/c/Means47N1Szn1",
