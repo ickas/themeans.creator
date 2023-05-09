26e5ad8a1cab85146e41f581d8d006f16093a2ec
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-05-09 14:03:23 +0100

    Update look-again.ts

--- app/lib/look-again.ts
+++ app/lib/look-again.ts
@@ -1,9 +1,27 @@
 export const lookAgainMeme1 = [
   // {
-  //   image: "image url",
-  //   name: "name",
-  //   artist: "artist",
-  //   url: "#0",
+  //   image: "https://d3lqz0a4bldqgf.cloudfront.net/images/scaled_x1000/0x33FD426905F149f8376e227d0C9D3340AaD17aF1/1.WEBP",
+  //   name: "6529Seizing",
+  //   artist: "6529er",
+  //   url: "https://opensea.io/assets/ethereum/0x33fd426905f149f8376e227d0c9d3340aad17af1/1/",
+  // },
+  // {
+  //   image: "https://i.seadn.io/gae/dwq6ejnAjwtNi1vio6YyQu8oshJJ76eqYwXJVnZb-mik-p8dVSbW2fW6EGcqqrlU5mHIk7mw6MIoGmdF_BdGIZzVaIX18-BN7HkRPA?auto=format&w=3840",
+  //   name: "Seizing",
+  //   artist: "The Means",
+  //   url: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432152972375883876/",
+  // },
+  // {
+  //   image: "https://d3lqz0a4bldqgf.cloudfront.net/images/scaled_x1000/0x33FD426905F149f8376e227d0C9D3340AaD17aF1/2.WEBP",
+  //   name: "SeizeJPGs",
+  //   artist: "6529er",
+  //   url: "https://opensea.io/assets/ethereum/0x33fd426905f149f8376e227d0c9d3340aad17af1/2/",
+  // },
+  // {
+  //   image: "https://i.seadn.io/gae/4SDzglwPW5CVldElEH3Vn2zXyl-FUIUvhoODBdHVOfrDszz-0EU-gir52asJOqdzt2ijskgUzIps1DViAb71ZmwhY4DUALoJ7PKOBg?auto=format&w=3840",
+  //   name: "SeizeJPEGs",
+  //   artist: "The Means",
+  //   url: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432154071887511652/",
   // },
 ];
 
