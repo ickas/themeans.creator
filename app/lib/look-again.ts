4cbf14b7e689199d960e59ff135f0145c121451e
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-05-09 13:47:04 +0100

    Update look-again.ts
    updated meme list to include one more meme theme and delete collector section

--- app/lib/look-again.ts
+++ app/lib/look-again.ts
@@ -3,7 +3,6 @@ export const lookAgainMeme1 = [
   //   image: "image url",
   //   name: "name",
   //   artist: "artist",
-  //   collector: "collector",
   //   url: "#0",
   // },
 ];
@@ -17,3 +16,4 @@ export const lookAgainMeme7 = [];
 export const lookAgainMeme8 = [];
 export const lookAgainMeme9 = [];
 export const lookAgainMeme10 = [];
+export const lookAgainMeme11 = [];
