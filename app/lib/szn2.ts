25255d6fdaf0ba875974f3687fa6fd736463d6a7
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-05-22 11:31:07 +0100

    Update szn2.ts

--- app/lib/szn2.ts
+++ app/lib/szn2.ts
@@ -262,6 +262,15 @@ export const szn2 = [
   },
   {
     image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/7f71376346ba3f0dad83b8e7dbcea760db11bfb14ac5f79b1c0f5f776028e75d.webp",
+    name: "6529 Fam Starter Pack",
+    url: "https://app.manifold.xyz/c/MeansSzn2Card30",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/36",
+    startDate: "2023-05-22",
+    endDate: "2023-11-22",
+  },
+  {
+    image:
       "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/a25dbcc594af8105e4ebc8d3180de02f61644f0a331807c8a3bcd4efc5f06405.jpg",
     name: "47 Means-in-1",
     url: "https://app.manifold.xyz/c/Means47N1Szn1",
