60bc9240eec2af618d150b51dc7e291c3ab1f2f7
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2023-06-02 12:33:25 +0100

    Update szn2.ts

--- app/lib/szn2.ts
+++ app/lib/szn2.ts
@@ -298,6 +298,15 @@ export const szn2 = [
   },
   {
     image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/c5f004f6151c181b69c0944b13180b71fac5cae6ef2b0441a83aa07207a2bdce.webp",
+    name: "No FOMO/COPE Today",
+    url: "https://app.manifold.xyz/c/MeansSzn2Card34",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/40",
+    startDate: "2023-06-02",
+    endDate: "2023-12-02",
+  },
+  {
+    image:
       "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/a25dbcc594af8105e4ebc8d3180de02f61644f0a331807c8a3bcd4efc5f06405.jpg",
     name: "47 Means-in-1",
     url: "https://app.manifold.xyz/c/Means47N1Szn1",
