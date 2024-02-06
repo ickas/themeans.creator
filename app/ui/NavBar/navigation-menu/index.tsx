355edecb28d73fc8c44216c2325a9ff963963409
Henrique Macedo <2805206+ickas@users.noreply.github.com>
2024-02-06 20:16:03 +0000

    feat: add new navigation bar

---
+++ app/ui/NavBar/navigation-menu/index.tsx
@@ -0,0 +1,78 @@
+import React, { LegacyRef } from "react";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import classNames from "classnames";
+import { CaretDownIcon } from "@radix-ui/react-icons";
+import "./styles.css";
+
+const ListItem = React.forwardRef(
+  (
+    {
+      className,
+      children,
+      title,
+      ...props
+    }: {
+      className: string;
+      children: React.ReactNode;
+      title: string;
+      [key: string]: any;
+    },
+    forwardedRef
+  ) => (
+    <li>
+      <NavigationMenu.Link asChild>
+        <a
+          className={classNames("ListItemLink", className)}
+          {...props}
+          ref={forwardedRef as LegacyRef<HTMLAnchorElement>}
+        >
+          <div className="ListItemHeading">{title}</div>
+          <p className="ListItemText">{children}</p>
+        </a>
+      </NavigationMenu.Link>
+    </li>
+  )
+);
+
+const NavigationMenuDemo = () => {
+  return (
+    <NavigationMenu.Root className="NavigationMenuRoot">
+      <NavigationMenu.List className="NavigationMenuList">
+        <NavigationMenu.Item>
+          <NavigationMenu.Link className="NavigationMenuLink" href="/">
+            Home
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger className="NavigationMenuTrigger">
+            Mint now <CaretDownIcon className="CaretDown" aria-hidden />
+          </NavigationMenu.Trigger>
+          <NavigationMenu.Content className="NavigationMenuContent">
+            <ul className="List one">
+              <ListItem href="/minting-now" title="The Means">
+                This is your means heads up page for mints.
+              </ListItem>
+              <ListItem href="/meaned-memes" title="Meaned Memes">
+                The memes and their means.
+              </ListItem>
+              <ListItem href="#0" title="Xpto Collection">
+                Ut exercitation sint pariatur eu duis ex culpa.
+              </ListItem>
+              <ListItem href="#0" title="XYZ Collection">
+                Ut exercitation sint pariatur eu duis ex culpa.
+              </ListItem>
+            </ul>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+        <NavigationMenu.Indicator className="NavigationMenuIndicator">
+          <div className="Arrow" />
+        </NavigationMenu.Indicator>
+      </NavigationMenu.List>
+      <div className="ViewportPosition">
+        <NavigationMenu.Viewport className="NavigationMenuViewport" />
+      </div>
+    </NavigationMenu.Root>
+  );
+};
+
+export default NavigationMenuDemo;
