4c16a38e8ccacc0a222e86034863b73811d544aa
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2024-02-07 16:25:50 +0000

    Update wdym.ts

--- app/lib/wdym.ts
+++ app/lib/wdym.ts
@@ -1,20 +1,143 @@
 export const wdym = [
-  {
+    {
     image:
-      "https://assets.manifold.xyz/image/upload/c_limit,q_50,w_400/844fba1eb1d98985059adc93043c6fe3249dee0d77d2eb05607842359d38073f.jpg",
-    name: "Survive Together",
-    url: "https://app.manifold.xyz/c/MeansSzn2Card1",
-    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/2",
-    startDate: "2023-03-01",
-    endDate: "2023-12-13",
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/0e03a4bb8cba74f21223e0c1d814c132d3b8876f21645e07394afebab22cf385.webp",
+    name: "WEB3 Inclusivity Problem-Solving",
+    url: "https://app.manifold.xyz/c/WEB3Inclusivity",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/109/",
+    startDate: "2023-12-23",
+    endDate: "2024-04-27",
   },
   {
     image:
-      "https://assets.manifold.xyz/image/upload/c_limit,q_50,w_400/fefd32551e54533d2335918d17ae6f1f6248ab83439e0f54bde45815ee416824.jpg",
-    name: "No FOMO, no COPE",
-    url: "https://app.manifold.xyz/c/MeansSzn2Card2",
-    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/3",
-    startDate: "2023-03-02",
-    endDate: "2023-12-13",
+      "",
+    name: "Open Road To Web 6529",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432170564561928247/",
+  },
+  {
+    image:
+      "",
+    name: "NFTs are DED",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432242032817733701/",
+  },
+  {
+    image:
+      "",
+    name: "NFT Adoption – How it started",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432184858213089322/",
+  },
+  {
+    image:
+      "",
+    name: "Look up, Anon",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432228838678200362/",
+  },
+  {
+    image:
+      "",
+    name: "GM MAXI",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432229938189828196/",
+  },
+  {
+    image:
+      "",
+    name: "Fight Outside - featuring Articulate",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432210146980528184/",
+  },
+  {
+    image:
+      "",
+    name: "FFSglyph6529",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432215644538667089/",
+  },
+  {
+    image:
+      "",
+    name: "DALL-E's",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432159569445650474/",
+  },
+    {
+    image:
+      "",
+    name: "Cereal is Art - featuring exlawyer.eth",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432159569445650474/",
+  },
+    {
+    image:
+      "",
+    name: "Sgt. Archimedes Pepe",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432165067003789354/",
+  },
+    {
+    image:
+      "",
+    name: "OMNet",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432158469934022695/",
+  },
+    {
+    image:
+      "",
+    name: "The Metaverse Opens Up",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432206848445644900/",
+  },
+    {
+    image:
+      "",
+    name: "Seize the Memers of Production",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432202450399133838/",
+  },
+    {
+    image:
+      "",
+    name: "Scent of GM",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432240933306105860/",
+  },
+    {
+    image:
+      "",
+    name: "Freedom to Love",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432220042585178172/",
+  },
+    {
+    image:
+      "",
+    name: "NONE Mieme - Rejected",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432245331352617002/",
+  },
+    {
+    image:
+      "",
+    name: "MEowMEow",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432192554794483808/",
+  },
+    {
+    image:
+      "",
+    name: "PAM Onboarding Pass",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432227739166572644/",
+  },
+    {
+    image:
+      "",
+    name: "Nakamoto Trustless Card",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432189256259600414/",
+  },
+    {
+    image:
+      "",
+    name: "Nakamoto Trustless Card - Gold Edition",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432200251375878174/",
+  },
+    {
+    image:
+      "",
+    name: "Nakamoto Trustless Card - Silver Edition",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432193654306111518/",
+  },
+    {
+    image:
+      "",
+    name: "Nakamoto Trustless Card - Black Metal Edition",
+    osUrl: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/89749659593897466821838212055205926765239153278206586590666432205748934017054/",
   },
 ];
