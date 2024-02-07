f0e29124344d67b5b64edcf13c7b9e26ac04dc75
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2024-02-07 15:53:07 +0000

    Update open-editions.ts

--- app/lib/open-editions.ts
+++ app/lib/open-editions.ts
@@ -1,20 +1,38 @@
 export const openEditions = [
-  {
+    {
     image:
-      "https://assets.manifold.xyz/image/upload/c_limit,q_50,w_400/844fba1eb1d98985059adc93043c6fe3249dee0d77d2eb05607842359d38073f.jpg",
-    name: "Survive Together",
-    url: "https://app.manifold.xyz/c/MeansSzn2Card1",
-    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/2",
-    startDate: "2023-03-01",
-    endDate: "2023-12-13",
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/0e03a4bb8cba74f21223e0c1d814c132d3b8876f21645e07394afebab22cf385.webp",
+    name: "WEB3 Inclusivity Problem-Solving",
+    url: "https://app.manifold.xyz/c/WEB3Inclusivity",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/109/",
+    startDate: "2023-12-23",
+    endDate: "2024-04-27",
+  },
+    {
+    image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_80,w_1024/b21803e570bd001c9d82dff51bf9eba209abb34b13222cf686b5ff5b16a2bfd6.webp",
+    name: "The Means, Year 1",
+    url: "https://app.manifold.xyz/c/TheMeansYear1",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/105",
+    startDate: "2023-12-08",
+    endDate: "2023-12-15",
   },
   {
     image:
-      "https://assets.manifold.xyz/image/upload/c_limit,q_50,w_400/fefd32551e54533d2335918d17ae6f1f6248ab83439e0f54bde45815ee416824.jpg",
-    name: "No FOMO, no COPE",
-    url: "https://app.manifold.xyz/c/MeansSzn2Card2",
-    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/3",
-    startDate: "2023-03-02",
-    endDate: "2023-12-13",
+      "https://assets.manifold.xyz/image/upload/c_limit,q_50,w_400/e083a37c9dbab1d383126148138b4f3215393c89aea772c7439e7e0570f1754c.jpg",
+    name: "Free Claim - Seize The Memes - Beeple ReMemes Elon Musk",
+    url: "https://app.manifold.xyz/c/MeansBeepleReMemesElon",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/19/",
+    startDate: "2023-04-04",
+    endDate: "2023-07-03",
+  },
+  {
+    image:
+      "https://assets.manifold.xyz/image/upload/c_limit,q_50,w_400/5bf248e91c47a93007c9d3c60f20c3906e360b3f51dd89411fe07d5d224f2a83.jpg",
+    name: "AGORA and RCS ReMeme Gallery on OM Visitor 'POAP'",
+    url: "https://app.manifold.xyz/c/AGORAVisitor",
+    osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/28/",
+    startDate: "2023-04-10",
+    endDate: "2023-07-10",
   },
 ];
