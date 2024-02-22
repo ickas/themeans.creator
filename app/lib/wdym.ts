369751bde7928ddbdeea4ba93406d63f0347fed2
TheMeansCreator <122363111+TheMeansCreator@users.noreply.github.com>
2024-02-22 13:41:45 +0000

    Update wdym.ts

--- app/lib/wdym.ts
+++ app/lib/wdym.ts
@@ -1,7 +1,7 @@
 export const wdym = [
     {
     image: "https://d1updk8hq321rl.cloudfront.net/optimized/72b6fa14513834cbf03533b2bbb8ad38302472a9f7c348b6b92895ba3ed73926/w_1024.webp",
-    name: "Sgt pepe World Freedom Card",
+    name: "Sgt Pepe World Freedom Card",
     url: "https://app.manifold.xyz/c/SgtPepeWorldFreedomCard",
     osUrl: "https://opensea.io/assets/ethereum/0x83a079036879f878f438e8df79a76dbd98ec23cf/126/",
     startDate: "2024-02-22",
